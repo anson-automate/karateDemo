0460dc8ae2bfaf01221b9ae505cb79ae8e894177
anson-automate <anson.paul.p@gmail.com>
2022-10-11 20:05:20 +0530

    Initial commit

---
+++ src/main/java/org/anson/karateDemo/App.java
@@ -0,0 +1,13 @@
+package org.anson.karateDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
